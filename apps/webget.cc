fa25b4ed7023abce6bc1465700929a5d44672c5b
JCLin0609 <as11675847@gmail.com>
2023-05-17 17:01:10 +0800

    Finish lab 0

--- apps/webget.cc
+++ apps/webget.cc
@@ -7,37 +7,55 @@
 
 using namespace std;
 
-void get_URL( const string& host, const string& path )
-{
-  cerr << "Function called: get_URL(" << host << ", " << path << ")\n";
-  cerr << "Warning: get_URL() has not been implemented yet.\n";
+void get_URL(const string &host, const string &path) {
+  TCPSocket sockTCP;
+  Address addr = Address(host, "http");
+  sockTCP.connect(addr);
+
+  std::string request =
+      "GET " + path + " HTTP/1.1\r\nHost: " + host + "\r\n\r\n";
+  sockTCP.write(request);
+  string buf;
+  vector<std::unique_ptr<std::string>> buffer;
+  while (true) {
+    sockTCP.read(buf);
+    cout << buf;
+    buffer.push_back(std::make_unique<std::string>(buf));
+    if (sockTCP.eof())
+      break;
+  }
+
+  // for ( const auto& strPtr : buffer ) {
+  //   std::cout << *strPtr << std::endl;
+  // }
+
+  sockTCP.close();
 }
 
-int main( int argc, char* argv[] )
-{
+int main(int argc, char *argv[]) {
   try {
-    if ( argc <= 0 ) {
+    if (argc <= 0) {
       abort(); // For sticklers: don't try to access argv[0] if argc <= 0.
     }
 
-    auto args = span( argv, argc );
+    auto args = span(argv, argc);
 
-    // The program takes two command-line arguments: the hostname and "path" part of the URL.
-    // Print the usage message unless there are these two arguments (plus the program name
-    // itself, so arg count = 3 in total).
-    if ( argc != 3 ) {
+    // The program takes two command-line arguments: the hostname and "path"
+    // part of the URL. Print the usage message unless there are these two
+    // arguments (plus the program name itself, so arg count = 3 in total).
+    if (argc != 3) {
       cerr << "Usage: " << args.front() << " HOST PATH\n";
       cerr << "\tExample: " << args.front() << " stanford.edu /class/cs144\n";
       return EXIT_FAILURE;
     }
 
     // Get the command-line arguments.
-    const string host { args[1] };
-    const string path { args[2] };
+    const string host{args[1]};
+    const string path{args[2]};
 
     // Call the student-written function.
-    get_URL( host, path );
-  } catch ( const exception& e ) {
+    get_URL(host, path);
+  } catch (const exception &e) {
     cerr << e.what() << "\n";
     return EXIT_FAILURE;
   }
--- src/byte_stream.cc
+++ src/byte_stream.cc
@@ -4,74 +4,85 @@
 
 using namespace std;
 
-ByteStream::ByteStream( uint64_t capacity ) : capacity_( capacity ) {}
+ByteStream::ByteStream(uint64_t capacity)
+    : capacity_(capacity), closed_(false), err_(false), total_push_num(0),
+      total_pop_num(0), // que(), // Method(deque)
+      que2()            // Method(string)
+{}
 
-void Writer::push( string data )
-{
-  // Your code here.
-  (void)data;
+void Writer::push(string data) {
+  if (is_closed() || data.empty() || err_)
+    return;
+
+  int len = min(available_capacity(), data.length());
+
+  // Method(deque):
+  // for (int i = 0; i < len; i++) {
+  //   que.push_back(data[i]);
+  // }
+
+  // Method(string):
+  que2 += data.substr(0, len);
+
+  total_push_num += len;
 }
 
-void Writer::close()
-{
-  // Your code here.
+void Writer::close() { closed_ = true; }
+
+void Writer::set_error() { err_ = true; }
+
+bool Writer::is_closed() const { return closed_; }
+
+uint64_t Writer::available_capacity() const {
+  // return capacity_ - que.size(); // Method(deque)
+  return capacity_ - que2.size(); // Method(string)
 }
 
-void Writer::set_error()
-{
-  // Your code here.
+uint64_t Writer::bytes_pushed() const { return total_push_num; }
+
+string_view Reader::peek() const {
+  // Method (deque): not work
+  // if (!que.empty()) {
+  //   std::string res;
+  //   res = que.front();
+  //   return res;
+  // } else
+  //   return "";
+
+  // Method (deque):  why
+  // return {&que.front(), 1};
+
+  // Method (String)
+  return que2;
 }
 
-bool Writer::is_closed() const
-{
-  // Your code here.
-  return {};
+bool Reader::is_finished() const {
+  return closed_ && que2.empty(); // Method(string)
+  // return closed_ && que.empty(); // Method(deque)
 }
 
-uint64_t Writer::available_capacity() const
-{
-  // Your code here.
-  return {};
+bool Reader::has_error() const { return err_; }
+
+void Reader::pop(uint64_t len) {
+  // int lengthh = min(len, que.size()); // Method(deque)
+  int lengthh = min(len, que2.size()); // Method(string)
+
+  // Method (deque)
+  // string tmp = "";
+  // for (int i = 0; i < lengthh; i++) {
+  //   tmp += que.front();
+  //   que.pop_front();
+  // }
+
+  // Method (string)
+  que2 = que2.substr(lengthh);
+
+  total_pop_num += lengthh;
 }
 
-uint64_t Writer::bytes_pushed() const
-{
-  // Your code here.
-  return {};
+uint64_t Reader::bytes_buffered() const {
+  // return que.size(); // Method(deque)
+  return que2.size(); // Method(string)
 }
 
-string_view Reader::peek() const
-{
-  // Your code here.
-  return {};
-}
-
-bool Reader::is_finished() const
-{
-  // Your code here.
-  return {};
-}
-
-bool Reader::has_error() const
-{
-  // Your code here.
-  return {};
-}
-
-void Reader::pop( uint64_t len )
-{
-  // Your code here.
-  (void)len;
-}
-
-uint64_t Reader::bytes_buffered() const
-{
-  // Your code here.
-  return {};
-}
-
-uint64_t Reader::bytes_popped() const
-{
-  // Your code here.
-  return {};
-}
+uint64_t Reader::bytes_popped() const { return total_pop_num; }
